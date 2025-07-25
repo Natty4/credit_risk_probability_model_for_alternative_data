ce50114e9c2297ef1c9f40218b78e85660e4f5c6
natty4 <natty7kt@gmail.com>
2025-07-25 03:43:52 +0300

    Task-6, fix: test case local/remote

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,29 +1,29 @@
 from fastapi.testclient import TestClient
 from api.main import app
+from unittest.mock import patch
+import numpy as np
 
 client = TestClient(app)
 
-def test_home():
-    res = client.get("/")
-    assert res.status_code == 200
-    assert res.json() == {"status": "API running ✅"}
+@patch("api.main.model.predict_proba")
+def test_predict(mock_predict):
+    # Simulate output of a real binary classifier (low risk vs high risk)
+    mock_predict.return_value = np.array([[0.08, 0.85]])
 
-def test_predict_low_risk():
-    payload = {
-        "Total_Amount": 1000.0,
-        "Avg_Amount": 250.0,
-        "Std_Amount": 10.0,
-        "Max_Amount": 300.0,
-        "Min_Amount": 200.0,
-        "Avg_Pos_Amount": 250.0,
-        "Total_Refunds": 0.0,
-        "Transaction_Count": 5,
-        "Recency": 5
-    }
-    res = client.post("/predict", json=payload)
-    assert res.status_code == 200
-    assert "risk_probability" in res.json()
-    assert "risk_label" in res.json()
-    
-    
-    
+    response = client.post("/predict", json={
+        "Total_Amount": 500.0,
+        "Avg_Amount": 100.0,
+        "Std_Amount": 20.0,
+        "Max_Amount": 200.0,
+        "Min_Amount": 50.0,
+        "Avg_Pos_Amount": 90.0,
+        "Total_Refunds": 2.0,
+        "Transaction_Count": 10,
+        "Recency": 15
+    })
+
+    assert response.status_code == 200
+    data = response.json()
+    assert "risk_probability" in data
+    assert "risk_label" in data
+    assert data["risk_label"] == "high_risk"
